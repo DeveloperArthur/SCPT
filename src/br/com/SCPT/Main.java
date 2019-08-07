6ae9ea86a19e8d8ef82e09e5daeccbd4c0a7bf53
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-08-07 15:09:36 -0300

    Update Main.java

--- src/br/com/SCPT/Main.java
+++ src/br/com/SCPT/Main.java
@@ -14,8 +14,8 @@ public class Main {
 		Trem cptm = new Trem();
 		Scanner sc = new Scanner(System.in);
 		Main recursao = new Main();
-		
-		while(true) {
+
+		while (true) {
 			System.out.println();
 			System.out.println();
 			System.out.println();
@@ -27,20 +27,20 @@ public class Main {
 			System.out.println("5-Status das portas");
 			System.out.println("6-Lotacao");
 			menu = sc.nextInt();
-			
-			if(menu == 1) {
+
+			if (menu == 1) {
 				cptm.andar();
-			}else if(menu == 2) {
+			} else if (menu == 2) {
 				cptm.parar();
-			}else if(menu == 3) {
+			} else if (menu == 3) {
 				cptm.abrirPortas(cptm.getPortas());
-			}else if(menu == 4) {
+			} else if (menu == 4) {
 				cptm.fecharPortas(cptm.getPortas());
-			}else if(menu == 5) {
+			} else if (menu == 5) {
 				cptm.statusPortas(cptm.getPortas());
-			}else if(menu == 6) {
+			} else if (menu == 6) {
 				cptm.lotar();
-			}else {
+			} else {
 				System.err.println("Digite uma das opcoes do menu!");
 			}
 		}
